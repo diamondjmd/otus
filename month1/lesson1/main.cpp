3824fdfed716b31060170a54501843ca24a2f4e2
Joseth Diamond <diamondjmd@gmail.com>
2023-06-29 20:53:47 +0300

    test2 branch added

--- month1/lesson1/main.cpp
+++ month1/lesson1/main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
  
  int main(){
-    std::cout << " hello world" << std::endl;
-    std::cout << "second line" << std::endl;
-    std::cout << "third line" << std::endl;
-
-    return 0;
+   std::cout << " hello world" << std::endl;
+   std::cout << "second line" << std::endl;
+   std::cout << "third line" << std::endl;
+   std::cout << "fourth line" << std::endl;
+   return 0;
  }
